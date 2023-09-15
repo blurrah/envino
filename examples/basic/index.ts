aa882b84a50c526a0f60ba6f8fe349f5c8169725
Boris Besemer <borisbesemer@gmail.com>
2023-09-15 15:54:44 +0200

    feat(parse): add support for env variable optiosn or shorthand
    This allows you to add additional options to an environment variable without giving up the shorthand notation.

--- examples/basic/index.ts
+++ examples/basic/index.ts
@@ -1,14 +1,22 @@
 import { config } from "dotenv";
 import { z } from "zod";
 import { createEnv } from "../..";
-const variables = config({ path: "./examples/basic/.env" });
 
-const parsedVariables = createEnv({
-  TEST_ENV_VARIABLE: {
-    check: z.string(),
-    unset: true,
+config({ path: "./examples/basic/.testenv" });
+
+const parsedVariables = createEnv(
+  {
+    TEST_ENV_VARIABLE: {
+      validate: z.string(),
+      unset: true,
+    },
+    TEST_BLA: z.string(),
   },
-});
+  {
+    disableProcessEnv: false,
+  }
+);
 
 console.log(parsedVariables); // { TEST_ENV_VARIABLE: "test" }
-console.log(process.env.TEST_ENV_VARIABLE);
+console.log(process.env.TEST_ENV_VARIABLE); // undefined
+console.log(process.env.TEST_BLA); // "test_bla_string"
--- index.ts
+++ index.ts
@@ -5,29 +5,38 @@ type Options = {
   envInput?: NodeJS.ProcessEnv;
 };
 
+type VariableObject = {
+  unset?: boolean;
+  validate: ZodType;
+};
+
 type Variables = {
-  [key: string]: {
-    unset?: boolean;
-    check: ZodType;
-  };
+  [key: string]: VariableObject | ZodType;
 };
 
 type ParsedVariables = Record<string, ZodType>;
 
+function checkIsVariableObject(
+  variable: Variables[keyof Variables]
+): variable is VariableObject {
+  return variable.hasOwnProperty("validate");
+}
+
 /**
- * Parses the variables object into a ZodObject ready for parsing
- * @param variables
- * @returns
+ * Parses the variables object into a ZodObject ready for parsing by zod.
+ *
+ * We allow both the direct ZodType and an object with additional options to be set on an environment variable.
+ * This function will create an object with just the zod types so that we can create a zod object.
  */
 function prepareVariables<TVariables extends Variables>(
   variables: TVariables
 ): ParsedVariables {
   return Object.entries(variables).reduce(
-    (acc: ParsedVariables, [key, value]) => {
-      acc[key] = value.check as ZodType;
+    (acc, [key, value]: [string, VariableObject | ZodType]) => {
+      acc[key] = checkIsVariableObject(value) ? value.validate : value;
       return acc;
     },
-    {}
+    {} as ParsedVariables
   );
 }
 
@@ -47,12 +56,13 @@ export function createEnv<TVariables extends Variables>(
   const parsed = zodObject.safeParse(env);
 
   if (!parsed.success) {
-    throw new Error(parsed.error.message);
+    console.error(parsed.error.flatten().fieldErrors);
+    throw new Error("Error parsing environment variables");
   }
 
   // Step 2: (Optional) Unset variables
   for (const [key, value] of Object.entries(variables)) {
-    if (value.unset) {
+    if (checkIsVariableObject(value) && value.unset) {
       delete process.env[key];
     }
   }
